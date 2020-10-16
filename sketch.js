e4e58230a77aa9e329a1a701eb997d81ee3ba694
Deleesha <jaindeleesha@gmail.com>
2020-10-16 15:55:18 +0530

    bg and score

--- sketch.js
+++ sketch.js
@@ -8,11 +8,13 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var score = 0;
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImage();
 }
+
 
 function setup(){
     var canvas = createCanvas(1200,400);
@@ -45,18 +47,25 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg){
     background(backgroundImg);
+    }
+
+    text("SCORE" + score , 1100,50);
     Engine.update(engine);
     //strokeWeight(4);
+
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +94,24 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBackgroundImage(){
+
+    //var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("http://worldtimeapi.org/api/timezone/America/Chicago");
+    var responseJSON = await response.json();
+    var mydatetime = responseJSON.datetime;
+    var hour = mydatetime.slice(11,13);
+    console.log(hour);
+
+    if(hour >= 6 && hour <=19){
+        bg = "sprites/bg.png";
+    }
+    else {
+        bg = "sprites/bg2.jpg";
+    }
+
+    backgroundImg = loadImage(bg);
+
 }
